57bdb67d79ae3f78a757ba453bdd2186969110c4
Samantha <87158482+Samanthas20@users.noreply.github.com>
2021-10-27 13:54:35 -0400

    Add files via upload

---
+++ ITS 250 Project.py
@@ -0,0 +1,4 @@
+#the following program is something that can be used to help students study.\
+#The program will work by calculating how many hours a certain student should study given the difficulty\
+#and the current grade within that class. The results should produce variying results that will help the student\
+#better acieve a good grade in each of their classes.#
